137b40bdd0742bac89a5835070f2121abdcb1043
PokeHacks69 <71925417+PokeHacks69@users.noreply.github.com>
2021-09-05 10:24:05 +0300

    Update HatMan Simulator.lua

--- HatMan Simulator.lua
+++ HatMan Simulator.lua
@@ -1,1 +1,4 @@
-LMAO SKID
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "LMAO SKID WHY U TRYING TO USE THIS SCRIPT NERD";
+    Text = "PUSSY FUCK OFF STOP TRYING TO USE MY SCRIPT";
+})
